3ca11619013e04f2f52b7501df6611ba2632ecc5
Satyam Yadav <innovationsnextgen250@gmail.com>
2025-06-13 22:45:26 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,7 @@
+API_ID = 123456  # Replace with your API ID
+API_HASH = "your_api_hash"
+BOT_TOKEN = "your_bot_token"
+SESSION_STRING = "your_session_string"
+
+# Add your Telegram user IDs here for sudo access
+SUDO_USERS = [123456789, 987654321]
